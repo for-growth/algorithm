3dcd017711a752a90d6117ebbce3b5a538dc0dd9
kunwoo park <scv74502@gmail.com>
2024-07-22 00:13:47 +0900

    6주차 풀이

---
+++ Week06/PGR_84512.py
@@ -0,0 +1,29 @@
+import sys
+from bisect import *
+
+
+def solution(word):
+    sys.setrecursionlimit(100000000)
+    vowels = 'AEIOU'
+    sb = []
+    words = []
+
+    def bt():
+        if len(sb) == 5:
+            words.append(''.join(sb))
+            return
+        elif sb:
+            words.append(''.join(sb))
+
+        for i in range(5):
+            sb.append(vowels[i])
+            bt()
+            sb.pop()
+
+    bt()
+    return bisect(words, word)
+
+
+if __name__ == '__main__':
+    print(solution("EIO"))
+
