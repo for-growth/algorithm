0c6fe03d6e3b3f7515c1f88bbe39cdc691d88909
0woy <qkrdbsdk1030@naver.com>
2024-07-24 16:26:58 +0900

    feat: 구현

--- Week07/공통/옹알이2/younA.java
+++ Week07/공통/옹알이2/younA.java
@@ -1,4 +1,4 @@
-package 옹알이1;
+package 옹알이2;
 
 public class younA {
     public static int solution(String[] babbling) {
@@ -6,7 +6,14 @@ public class younA {
         int count=0;
         for(String babble: babbling){
             for(int i=0;i<words.length;i++){
-               babble= babble.replaceAll(words[i]," ");
+                int current = babble.length();
+                int after = babble.replaceAll(words[i]+words[i],"").length();
+                if(current!=after){
+                    break;
+                }
+                else{
+                    babble = babble.replaceAll(words[i]," ");
+                }
             }
             if(babble.replaceAll(" ","").equals("")){
                 count++;
@@ -17,6 +24,6 @@ public class younA {
         return count;
     }
     public static void main(String[] args) {
-        solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"});
+        solution(new String[]{"aya", "yee", "u", "maa"});
     }
 }
